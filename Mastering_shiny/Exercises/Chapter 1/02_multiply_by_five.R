4d68f3f5ecc5fe8fd5ed2b67134bad6ac7216983
Joe PS <61272205+Joeeeps@users.noreply.github.com>
2024-08-05 16:51:21 +0100

    Update 02_multiply_by_five.R

---
+++ Mastering_shiny/Exercises/Chapter 1/02_multiply_by_five.R
@@ -0,0 +1,16 @@
+#Create an app that lets a user input a number (1-50) and then outputs number * 5
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput("x", label = "If x is", min = 1, max = 50, value = 30),
+  "then x times 5 is",
+  textOutput("product")
+)
+
+server <- function(input, output, session) {
+  output$product <- renderText({
+	input$x * 5 #solution was to add "input$" prior to "x", as it needed to be linked to the input
+  })
+}
+
+shinyApp(ui, server)
