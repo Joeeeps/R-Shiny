5b69675ae247d36282bcbd65f5568686fe800bda
Joe PS <61272205+Joeeeps@users.noreply.github.com>
2024-08-06 15:39:20 +0100

    Add files via upload

---
+++ Mastering_shiny/Exercises/Chapter 3/01_fix_the_errors_A.R
@@ -0,0 +1,27 @@
+#given the UI, fix the simple errors found in the three server functions below
+library(shiny)
+
+ui <- fluidPage(
+  textInput("name", "What's your name?"),
+  textOutput("greeting")
+)
+
+server1 <- function(input, output, server) {
+  output$greeting <- renderText({
+    paste0("Hello ", input$name)
+  })
+}
+
+shinyApp(ui, server1) #server1 also works, doesn't have to be strictly server
+
+#original  
+
+#server1 <- function(input, output, server) {
+#  input$greeting <- renderText(paste0("Hello ", name)) 
+#
+
+#solution
+#change input$greeting to output$greeting
+#add extra {} for the render text
+#change name to input$name
+
